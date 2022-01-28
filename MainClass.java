094b0c52883e4aca81c9b8133f36302804553d97
sannu89 <61963603+sannu89@users.noreply.github.com>
2022-01-28 11:34:34 +0530

    Create MainClass.java
    The Main class added

---
+++ MainClass.java
@@ -0,0 +1,31 @@
+package git;
+import java.util.Scanner;
+public class MainClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("""
+                                
+                +---------------------------------+
+                |    WELCOME TO LOCKEDme.com      |
+                |---------------------------------|
+                |    Developer:Shanmukh Talawar   |
+                |             :shtalawa@cisco.com |
+                +---------------------------------+""");
+        while (true) {
+            int option = isRight.checkInput();
+            switch (option) {
+                System.out.println("""
+                                            
+                        |Choose option:
+                        |   1.Retrieve Files
+                        |   2.Business Level Operations
+                        |   3.Exit Application
+                        """);
+                System.out.println("Working Directory = " + System.getProperty("user.dir"));
+    
+
+                }
+            }
+        }
+    }
+}
